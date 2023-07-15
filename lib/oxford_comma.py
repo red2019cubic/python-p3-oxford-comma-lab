33e244a30aac4838d1179ff5a226d5857a476104
Red <red.sarouf2014@gmail.com>
2023-07-15 15:47:34 -0400

    add python oxford comma lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    new_items = []
+    for i in range(len(items) - 1):
+        new_items.append(items[i])
+    new_string = ", ".join(new_items) 
+    if len(items) == 1:
+        n_string = items[0]
+    elif len(items) == 2: 
+       n_string = new_string +  " and " + items[len(items) - 1]
+    elif len(items) > 2:
+       n_string = new_string +  ", and " + items[len(items) - 1]
+   
+    return n_string
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
